6e5a97b7f6b383f8ec451876063cfb805dee0c62
Davi <adavi9920@gmail.com>
2023-09-10 12:56:16 -0300

    entendendo resource bundle

---
+++ LearningJava/src/main/java/com/myproject/learningjava/classes_utilitarias/Sresource_bundle/teste/ResourceBrundleTest01.java
@@ -0,0 +1,23 @@
+package com.myproject.learningjava.classes_utilitarias.Sresource_bundle.teste;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class ResourceBrundleTest01 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * É uma forma de internacionalizar as mensagens do seu site, ou seja, transferir o conteúdo
+		 * textual do site baseado em uma língua/localização.
+		 * */
+		
+		 ResourceBundle resource = ResourceBundle.getBundle("message_en_US", new Locale("en", "US"));
+		 System.out.println(resource.getString("good.morning"));
+		 System.out.println("------------------------------------------------------------");
+		 //resource = ResourceBundle.getBundle("message_pt_BR", new Locale("pt", "BR"));
+		 //System.out.println(resource.getString("good.morning"));
+		 
+	}
+
+}
